cae051ce7dfb817aa8f9fa5bf2b6284a89cc6718
felixh <felixhr2000@gmail.com>
2023-06-21 10:27:14 -0500

    notify when OpenAI key is not set

--- src/services/resources/OpenAI.ts
+++ src/services/resources/OpenAI.ts
@@ -2,6 +2,10 @@ import { Configuration, OpenAIApi } from "openai"
 
 const OPENAI_KEY = process.env.OPENAI_KEY
 
+if (!OPENAI_KEY) {
+  console.error("OpenAI API key not set in environment variables.")
+}
+
 const configuration = new Configuration({
   apiKey: OPENAI_KEY,
 })
